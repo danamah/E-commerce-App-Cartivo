abf175983eacbb45a24c614b1ffe2057ab267470
Dana-Madny <dana.madny@gmail.com>
2026-02-22 16:14:05 +0200

    fourth commit

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,8 +1,28 @@
 import { decode } from "next-auth/jwt";
 import { cookies } from "next/headers";
 
-export async function getUserToken(){
-    const decodedToken = (await cookies()).get("next-auth.session-token")?.value
-    const token = await decode({token: decodedToken, secret:process.env.AUTH_SECRET!})
+export async function getUserToken() {
+    // const decodedToken = (await cookies()).get("next-auth.session-token")?.value
+    const cookieStore = await cookies();
+    const decodedToken =
+        cookieStore.get("__Secure-next-auth.session-token")?.value ||
+        cookieStore.get("next-auth.session-token")?.value;
+    const token = await decode({ token: decodedToken, secret: process.env.AUTH_SECRET! })
     return token?.token
 }
+
+
+// import { getToken } from "next-auth/jwt";
+
+// export async function getUserToken() {
+//   const token = await getToken({
+//     req: {
+//       headers: Object.fromEntries(
+//         (await import("next/headers")).headers()
+//       )
+//     } as any,
+//     secret: process.env.NEXTAUTH_SECRET,
+//   });
+
+//   return token?.token;
+// }
