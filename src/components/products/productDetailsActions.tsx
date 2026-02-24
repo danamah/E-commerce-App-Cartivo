12e606011baebbbb0d82bb4e9a29f69bdf596ae5
Dana-Madny <dana.madny@gmail.com>
2026-02-24 03:31:55 +0200

    Product-Detail localization&Loadings commit

--- src/components/products/productDetailsActions.tsx
+++ src/components/products/productDetailsActions.tsx
@@ -11,6 +11,7 @@ import { toast } from "sonner"
 import { ProductsI } from "@/types/products"
 import { useTranslations } from "next-intl"
 import { Spinner } from "../ui/spinner"
+import { useSession } from "next-auth/react"
 
 export default function ProductDetailsActions({ product }: { product: ProductsI }) {
     const [count, setCount] = useState(1)
@@ -18,10 +19,19 @@ export default function ProductDetailsActions({ product }: { product: ProductsI 
     const { handleWishList } = useContext(wishListContext)
     const [isLoading, setIsLoading] = useState(false)
     const [isWishLoading, setIsWishLoading] = useState(false)
-     const t = useTranslations("productDetails");
+    const { data: session } = useSession()
+    const t = useTranslations("productDetails");
     async function handleAddToCart() {
         try {
             setIsLoading(true)
+            await new Promise(res => setTimeout(res, 400))
+            if (!session) {
+                const cart = JSON.parse(localStorage.getItem("cart") || "[]")
+                cart.push(product._id)
+                localStorage.setItem("cart", JSON.stringify(cart))
+                toast.success("Added to guest cart ✅")
+                return
+            }
             const res = await addToCart(product._id)
             if (res.status === "success") {
                 await updateCartProductCount(product._id, count)
@@ -30,13 +40,21 @@ export default function ProductDetailsActions({ product }: { product: ProductsI 
             }
         } catch {
             toast.error("Error")
-        }finally{
+        } finally {
             setIsLoading(false)
         }
     }
     async function handleAddToWishList() {
-        setIsWishLoading(true)
         try {
+            setIsWishLoading(true)
+            await new Promise(res => setTimeout(res, 400))
+            if (!session) {
+                const wishList = JSON.parse(localStorage.getItem("wishList") || "[]")
+                wishList.push(product._id)
+                localStorage.setItem("wishList", JSON.stringify(wishList))
+                toast.success("Added to wish list ✅")
+                return
+            }
             const res = await addToWishList(product._id)
             if (res.status === "success") {
                 toast.success("Added to wishlist ❤️")
@@ -44,7 +62,7 @@ export default function ProductDetailsActions({ product }: { product: ProductsI 
             }
         } catch {
             toast.error("Error")
-        }finally{
+        } finally {
             setIsWishLoading(false)
         }
     }
@@ -64,12 +82,12 @@ export default function ProductDetailsActions({ product }: { product: ProductsI 
             </div>
             <div className="grid grid-cols-12 gap-2">
                 <Button className="col-span-12 md:col-span-6" onClick={handleAddToCart}>
-                    {isLoading ? <Spinner/> : <>
-                    <ShoppingCart size={18} /> {t("addBtn")}
+                    {isLoading ? <Spinner /> : <>
+                        <ShoppingCart size={18} /> {t("addBtn")}
                     </>}
                 </Button>
                 <Button className="col-span-12 md:col-span-6" onClick={handleAddToWishList}>
-                    {isWishLoading ? <Spinner/> : <><Heart size={18} /> {t("wishBtn")}</>}
+                    {isWishLoading ? <Spinner /> : <><Heart size={18} /> {t("wishBtn")}</>}
                 </Button>
             </div>
         </div>
